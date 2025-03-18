da6e161eac11a4bf7abffb7708d3fe486ed7e45d
Praneeth Byna <praneethbyna@gmail.com>
2025-03-18 10:59:50 -0500

    checking ci/cd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 p-4 text-white">
       <div className="container mx-auto">
-        <h1 className="text-xl font-semibold">Document Repository</h1>
+        <h1 className="text-xl font-semibold">
+          Document Repository - Opensearch
+        </h1>
       </div>
     </nav>
   );
